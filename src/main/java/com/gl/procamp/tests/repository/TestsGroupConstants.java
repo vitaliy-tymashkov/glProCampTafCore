3fceaaef54b74ad43298f60c8999780e794669d3
Vitalii Tymashkov <vitalii.tymashkov.ctr@sabre.com>
2021-05-16 12:24:34 +0200

    Test Framework Skeleton added

--- src/main/java/com/gl/procamp/tests/repository/TestsGroupConstants.java
+++ src/main/java/com/gl/procamp/tests/repository/TestsGroupConstants.java
@@ -2,5 +2,5 @@ package com.gl.procamp.tests.repository;
 
 public class TestsGroupConstants {
     public final static String SMOKE_TEST = "SmokeTest";
-    public final static String COSMOS_ID= "CosmosId";
+    public final static String COSMOS_ID = "CosmosId";
 }
